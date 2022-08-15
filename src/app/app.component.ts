d7dbcd8aae0484be520a533e9486623da4fce0a6
Ivan Shparkovich <shparkovich.i@medlinx.online>
2022-08-15 21:14:36 +0700

    govno commit one

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'govno';
+  public test(): void {
+    console.log('void');
+  }
 }
